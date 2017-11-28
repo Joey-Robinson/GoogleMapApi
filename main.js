8a0c39de123e54c68d2d3c4531e2f2e641fa486c
Gosu <theamazingness@gmail.com>
2017-11-27 19:50:55 -0600

    first commit. i'll try to change to fully functioning es6 if i remember

---
+++ main.js
@@ -0,0 +1,53 @@
+// const APIKEY = `AIzaSyAox9E0MWF4Tmm5z9vhDnInnwRFM_amU2I`;
+// const googleApi = `https://maps.googleapis.com/maps/api/js?key=${APIKEY}&callback=initMap`;
+// https://maps.googleapis.com/maps/api/js?key=AIzaSyAox9E0MWF4Tmm5z9vhDnInnwRFM_amU2I&callback=initMap
+// let marker = new google.maps.Marker({
+//     position: {},
+//     map: map
+// });
+// let infoWindow = new google.maps.InfoWindow({
+//     content: `<h1>${this.position}</h1>`
+// });
+// marker.addListener('click', function() {
+//     infoWindow.open(map, marker);
+// });
+//
+//
+//Above code doesn't work. I couldn't push the API to the HTML without it actually being there. 
+
+initMap = () => {
+    let options = {
+        zoom: 8,
+        center: {lat: 42, lng: -71}
+    }
+    let map = new google.maps.Map(document.getElementById('map'), options);
+    google.maps.event.addListener(map, 'click', function(e) {
+        addMarker({coords:e.latLng});
+    });
+    let markers = [];
+    for (let i = 0; i < markers.length; i++) {
+        addMarker(markers[i]);
+    }
+    addMarker = (props) => {
+        let marker = new google.maps.Marker({
+            position: props.coords,
+            map: map
+        });
+        if (props.iconImage) {
+            marker.setIcon(props.iconImage);
+        }
+        if (props.content) {
+            let infoWindow = new google.maps.InfoWindow({
+                content: props.content
+            });
+            marker.addListener('click', function() {
+                infoWindow.open(map, marker);
+            });
+        }
+    }
+}
+
+
+
+
+
